90840ecbf9ee189d557d33a6c2652a21ffab9cbc
ashishkk22 <ashishkachhadiya22@gmail.com>
2024-08-18 22:30:26 +0530

    feat: add react hot toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Providers from "@/app/providers";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,8 @@ export default function RootLayout({
       <body className={`min-h-screen flex flex-col ${inter.className}`}>
         <Providers>
           <Navbar />
+          <Toaster position="top-center" />
+
           <div className="flex-1 flex">
             <div className="w-full">{children}</div>
           </div>
